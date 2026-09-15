886126bf07a8c0cb7037ff94d302f4aa5f8fd94e
Artur Neruk <anerukpfk@gmail.com>
2026-09-15 15:22:32 +0300

    Implement Genre and Actor models and CRUD operations

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
 import init_django_orm  # noqa: F401
-
 from django.db.models import QuerySet
+
+from db.models import Actor, Genre
 
 
 def main() -> QuerySet:
-    pass
+    # 1. Create
+    genres_to_create = ["Western", "Action", "Dramma"]
+    for genre_name in genres_to_create:
+        Genre.objects.create(name=genre_name)
+
+    actors_to_create = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+    for first_name, last_name in actors_to_create:
+        Actor.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+    # 2. Update
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(
+        first_name="George",
+        last_name="Klooney",
+    ).update(last_name="Clooney")
+    Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves",
+    ).update(
+        first_name="Keanu",
+        last_name="Reeves",
+    )
+
+    # 3. Delete
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    # 4. Return
+    return Actor.objects.filter(
+        last_name="Smith",
+    ).order_by("first_name")
+
+
+if __name__ == "__main__":
+    main()
